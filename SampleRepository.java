d40415ef36b5f2e1bb2bfd11bf7cd677db016b8f
Sriram T <114211951+Sriramt936@users.noreply.github.com>
2025-09-27 15:59:37 +0530

    Delete SampleRepository.java

--- SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serviceapi.primary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.serviceapi.model.primary.Sample;
-
-public interface SampleRepository extends JpaRepository<Sample, Integer> { 
-	
-}
